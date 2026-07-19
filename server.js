a4896751fca687d373fa9c28e9a259afa43a065f
Gabor Kiss <kissgm92@gmail.com>
2026-07-19 17:15:31 +0200

    date update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const formats = {
   classic: {
     topcardsPath: "./data/topcards-classic.csv",
     orangeCardsPath: "./data/orange-classic.csv",
-    period: "2026.06.15 - 2026.07.04."
+    period: "2026.06.15 - 2026.07.19."
   },
   tm: {
     topcardsPath: "./data/topcards-tm.csv",
